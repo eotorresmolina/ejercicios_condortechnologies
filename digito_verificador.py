750f7a0b38faa7d5c0f8e80c7c9c7a8db846b9cb
eotorresmolina <64936281+eotorresmolina@users.noreply.github.com>
2021-01-15 00:35:51 -0300

    Update

--- digito_verificador.py
+++ digito_verificador.py
@@ -1,13 +1,31 @@
+'''
+Dígito Verificador [Python]
+---------------------------
+Autor: Torres Molina, Emmanuel O.
+Version: 1.1
+Descripción:
+Programa que buscar calcular un dígito verificador.
+En este caso, para la resolución se hizo uso de la 
+librería Numpy aprovechando sus ventajas.
+'''
+
+__author__ = "Torres Molina, Emmanuel O."
+__email__ = "emmaotm@gmail.com"
+__version__ = "1.1"
+
+
 import numpy as np
 
 
-def inv_dgts(digits):
+def inv_dgts(number):
     '''
-    Función que Invierte el orden de los digitos
-    contenidos en la lista "digits" recibida como parametro 
-    en sentido inverso
+    Función que Invierte el orden de los dígitos
+    contenidos en el entero "number" recibida como parametro.
     Devuelve un array con los digitos invertidos.
     '''
+
+    # Convierto el número entero en una lista de string cuyos elementos son los dígitos
+    digits = list(str(number))
 
     # Recorro la lista en sentido inverso y la convierto en un numpy array.
     dgts_inv = np.asanyarray(digits[::-1], dtype=np.int64)
@@ -20,19 +38,18 @@ def mult_digbysec(digits):
     Función que recibe un array y realiza 
     una multiplicación con la siguiente 
     secuencia de enteros [2, 3, 4, 5, 6, 7].
-    Retorna la suma de los elementos multiplicados.
+    Retorna un número entero igual a la suma de 
+    los elementos multiplicados.
     '''
 
     # Creo un array vacío para contener la secuencia
     # de números.
     sec = np.array([], dtype=np.int64)
 
-    # Realizo la diferencia entre el número de digitos y la
-    # longitud de la secuencia.
     diff = len(digits) - len(sec)
 
-    # Realizo un concatenado de la secuencia en caso de que
-    # de que la misma haya acabado ---> cant_digitos > nro_digitos_secuencia
+    # Concateno la secuencia con ella misma
+    # solo si ---> cant_digitos > nro_digitos_secuencia
     # Repito el proceso hasta que ambos array tenga la misma cant. de elementos.
     while (diff != 0):
 
@@ -57,7 +74,6 @@ def verificar(value):
     Si es 11 se intercambia por 0, si es 10 por 1.
     Retorna el valor intercambiado.
     '''
-
     value %= 11 # Obtengo el Resto de la división entre el valor y 11.
     result = 11 - value # Le resto de 11 al resultado anterior.
 
@@ -70,13 +86,28 @@ def verificar(value):
     return result 
 
 
-if __name__ == "__main__":
+def digito_verificador(numero):
+    '''
+    Función principal que toma un número entero
+    y mediante ciertos procesos
+    calcula el dígito verificador.
+    Retorna el valor del dígito verificador.
+    '''
 
-    digitos = list(str(input('\nPor favor Ingrese Dígitos "Enteros" que desee: ')))
-    
-    dgts_inv = inv_dgts(digitos)
+    dgts_inv = inv_dgts(numero)
     value = mult_digbysec(dgts_inv)
     result = verificar(value)
 
-    print('Resultado de la Verificación: {}\n\n'.format(result))
+    return result
+
+
+if __name__ == "__main__":
+
+    # Realizo el testeo del correcto funcionamiento del programa.
+
+    numero = int(input('\nPor favor Ingresar un Número "Entero": '))
+    
+    dgt = digito_verificador(numero)
+
+    print('Resultado de la Verificación: {}\n\n'.format(dgt))
 
